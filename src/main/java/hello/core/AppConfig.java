505125301265cba1cd4530e268f2a03e2cc86e28
Ik Hyeon Cho <andantej99@ajou.ac.kr>
2023-08-05 20:46:31 +0900

    05_05_@Configuration과 Singleton 1. AppConfig 호출 횟수를 통한 spring의 singleton 보장 공부

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,14 +17,17 @@ public class AppConfig {
     // @Bean을 method에 붙이면 Spring Container에 등록된다.
     @Bean
     public MemberService memberService() {                              // 이런 방식 = 생성자 주입
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderServiceImpl orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
@@ -32,5 +35,6 @@ public class AppConfig {
         return new RateDiscountPolicy();
     }
 }
-
-
+/*
+* 생각해보면 memberService, orderService 각각 1번씩 memberRepository 3번 호출될 것 같다
+* 하지만 현실은 각 메서드는 한번씩만 호출된다 = spring은 singleton을 보장해준다*/
